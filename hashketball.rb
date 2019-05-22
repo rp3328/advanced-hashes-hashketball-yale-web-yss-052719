f0b3ad9f151f8acf115226a41a704cdef655cd93
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-22 09:39:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,8 +2,8 @@
 
 def game_hash
   {
-    :home  => {:team_name => "", 
-      :colors => [], 
+    :home  => {:team_name => "",
+      :colors => [],
       :players => {
         "" => {:number, :shoe, :points, :rebounds, :assists, :steals, :blocks, :slam_dunks}
         }

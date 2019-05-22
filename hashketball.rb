33a4e2c98294d2d5840192a7dbca205198089dae
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-22 09:51:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,7 @@
 def game_hash
   {
     :home  => {:team_name => "Brooklyn Nets",
-      :colors => [Black, White],
+      :colors => ["Black", "White"],
       :players => {
         "Alan Anderson" => {:number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1},
         "Reggie Evans" => {:number => 30, :shoe => 14, :points => 12, :rebounds => 12, :assists => 12, :steals => 12, :blocks => 12, :slam_dunks => 7},
@@ -13,7 +13,7 @@ def game_hash
         }
       },
     :away => {:team_name => "Charlotte Hornets",
-      :colors => [Turquoise, Purple],
+      :colors => ["Turquoise", "Purple"],
       :players => {
         "Jeff Adrien" => {:number => 4, :shoe => 18, :points => 10, :rebounds => 1, :assists => 1, :steals => 2, :blocks => 7, :slam_dunks => 2},
         "Bismack Biyombo" => {:number => 0, :shoe => 16, :points => 12, :rebounds => 4, :assists => 7, :steals => 7, :blocks => 15, :slam_dunks => 10},

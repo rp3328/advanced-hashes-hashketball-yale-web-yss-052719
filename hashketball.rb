8219947eb2444fcec3994445f5c1b477ce21b2ea
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-22 09:45:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,8 +5,8 @@ def game_hash
     :home  => {:team_name => "Brooklyn Nets",
       :colors => [Black, White],
       :players => {
-        "Alan Anderson" => {:number => 4, :shoe => 18, :points => 10, :rebounds => 1, :assists => 1, :steals => 2, :blocks => 7, :slam_dunks => 2},
-        "Reggie Evans" => {:number=>, :shoe, :points, :rebounds, :assists, :steals, :blocks, :slam_dunks},
+        "Alan Anderson" => {:number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1},
+        "Reggie Evans" => {:number => 30, :shoe => 14, :points => 12, :rebounds => 12, :assists => 12, :steals => 12, :blocks => 12, :slam_dunks => 7},
         "Brook Lopez" => {:number, :shoe, :points, :rebounds, :assists, :steals, :blocks, :slam_dunks},
         "Mason Plumlee" => {:number, :shoe, :points, :rebounds, :assists, :steals, :blocks, :slam_dunks},
         "Jason Terry" => {:number, :shoe, :points, :rebounds, :assists, :steals, :blocks, :slam_dunks},

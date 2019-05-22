c73cbe4c41f617306eded2090c1b40d0dbcf4026
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-22 09:44:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,7 +6,7 @@ def game_hash
       :colors => [Black, White],
       :players => {
         "Alan Anderson" => {:number => 4, :shoe => 18, :points => 10, :rebounds => 1, :assists => 1, :steals => 2, :blocks => 7, :slam_dunks => 2},
-        "Reggie Evans" => {:number, :shoe, :points, :rebounds, :assists, :steals, :blocks, :slam_dunks},
+        "Reggie Evans" => {:number=>, :shoe, :points, :rebounds, :assists, :steals, :blocks, :slam_dunks},
         "Brook Lopez" => {:number, :shoe, :points, :rebounds, :assists, :steals, :blocks, :slam_dunks},
         "Mason Plumlee" => {:number, :shoe, :points, :rebounds, :assists, :steals, :blocks, :slam_dunks},
         "Jason Terry" => {:number, :shoe, :points, :rebounds, :assists, :steals, :blocks, :slam_dunks},
@@ -16,7 +16,7 @@ def game_hash
     :away => {:team_name => "Charlotte Hornets",
       :colors => [Turquoise, Purple],
       :players => {
-        "Jeff Adrien" => {:number, :shoe, :points, :rebounds, :assists, :steals, :blocks, :slam_dunks}
+        "Jeff Adrien" => {:number => 4, :shoe => 18, :points => 10, :rebounds => 1, :assists => 1, :steals => 2, :blocks => 7, :slam_dunks => 2},
         "Bismack Biyombo" => {:number, :shoe, :points, :rebounds, :assists, :steals, :blocks, :slam_dunks},
         "DeSagna Diop" => {:number, :shoe, :points, :rebounds, :assists, :steals, :blocks, :slam_dunks},
         "Ben Gordon" => {:number, :shoe, :points, :rebounds, :assists, :steals, :blocks, :slam_dunks},
